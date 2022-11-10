b355f1d7e7e26071d574ec666f936fcaf5c37682
Steff <steff.1@gmail.com>
2022-11-10 13:07:20 +0100

    Version 3.3

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const apprenant = ["Delphine", "Socunthy", "Laure", "Sandrine", "Arnaud", "Alexandre", "Jean-François", "Jean-Christophe", "Jean-Sébastien", "Terry", "Jessie", "Sébastien", "Pierre", "Kévin", "Johann", "Stéphane"]
+const button_random_apprenant = document.querySelector("#random_apprenant")
+const button_init_state = document.querySelector(value)
+const list_apprenant = ["Delphine", "Socunthy", "Laure", "Sandrine", "Arnaud", "Alexandre", "Jean-François", "Jean-Christophe", "Jean-Sébastien", "Terry", "Jessie", "Sébastien", "Pierre", "Kévin", "Johann", "Stéphane"]
 
-function randomApprenant() {
-    var randomApprenant = apprenant[Math.floor(Math.random()*apprenant.length)];
-    document.getElementById('randomApprenant').value = randomApprenant
+function random_apprenant() {
+    button_random_apprenant.setAttribute("value", Math.floor(Math.random()*apprenant.length));
+    document.getElementById('randomApprenant').value = random_pprenant
 }
